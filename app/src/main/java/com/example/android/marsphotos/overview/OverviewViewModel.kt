9564a768c226659f84dcf402fa228266d0fc5656
Sajini87 <sadanand.sajini@gmail.com>
2021-12-05 06:33:23 +0530

    Mars Api changes

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -19,17 +19,31 @@ package com.example.android.marsphotos.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.marsphotos.network.MarsApi
+import com.example.android.marsphotos.network.MarsPhoto
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
+enum class MarsApiStatus{
+    LOADING,
+    ERROR,
+    DONE
+}
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
  */
 class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData that stores the status of the most recent request
-    private val _status = MutableLiveData<String>()
+    private val _status = MutableLiveData<MarsApiStatus>()
 
     // The external immutable LiveData for the request status
-    val status: LiveData<String> = _status
+    val status: LiveData<MarsApiStatus> = _status
+
+    private val _photos = MutableLiveData<List<MarsPhoto>>()
+    val photos:LiveData<List<MarsPhoto>> get() = _photos
     /**
      * Call getMarsPhotos() on init so we can display status immediately.
      */
@@ -42,6 +56,16 @@ class OverviewViewModel : ViewModel() {
      * [MarsPhoto] [List] [LiveData].
      */
     private fun getMarsPhotos() {
-        _status.value = "Set the Mars API status response here!"
+        viewModelScope.launch {
+            try {
+                _status.value = MarsApiStatus.LOADING
+                delay(2000)
+                _photos.value = MarsApi.retrofitService.getPhotos()
+                _status.value = MarsApiStatus.DONE
+            }catch (e:Exception){
+                _status.value = MarsApiStatus.ERROR
+                _photos.value = listOf()
+            }
+        }
     }
 }
